332c05267e4e3373869058579ae4c993f900eb5f
Tyler Mayfield <Tyler@test.com>
2017-06-12 17:21:38 -0700

    Users with photos persisting

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UsersRequest;
 use App\Http\Requests;
 use App\User;
 use App\Role;
+use App\Photo;
 
 class AdminUsersController extends Controller
 {
@@ -45,9 +46,24 @@ class AdminUsersController extends Controller
     {
         //
         //return $request->all();
-        User::create($request->all());
+        // User::create($request->all());
 
-        return redirect('/admin/users');
+        // return redirect('/admin/users');
+
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
     }
 
     /**
